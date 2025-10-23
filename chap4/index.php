0f85adfeb35c91ffb37e6f99f8a047a7f73e6c5f
Florent <florent.email@email.com>
2025-10-23 17:56:50 +0200

    update chap4

--- chap4/index.php
+++ chap4/index.php
@@ -149,3 +149,77 @@ for ($o = 1; $o <= 5; $o++) : ?>
       <img src="img/image<?= $o ?>.png" alt="Image <?= $o ?>" width="100">
 <?php
 endfor;
+
+/*
+Créez une fonction qui transforme une chaine de caractères en une suite de chiffres selon le codage du clavier téléphonique : 
+ABC ⇒ 2
+DEF ⇒ 3
+GHI ⇒ 4
+JKL ⇒ 5
+MNO ⇒ 6
+PQRS ⇒ 7
+TUV ⇒ 8
+WXYZ ⇒ 9
+
+Exemple : Le mot hello affichera 43556
+*/
+
+
+function telephone(string $mot)
+{
+      $mot = strtoupper($mot);
+      $lettres = str_split($mot);
+      foreach ($lettres as $lettre) {
+            switch ($lettre) {
+                  case 'A':
+                  case 'B':
+                  case 'C':
+                        echo '2';
+                        break;
+                  case 'D':
+                  case 'E':
+                  case 'F':
+                        echo '3';
+                        break;
+                  case 'G':
+                  case 'H':
+                  case 'I':
+                        echo '4';
+                        break;
+                  case 'J':
+                  case 'K':
+                  case 'L':
+                        echo '5';
+                        break;
+                  case 'M':
+                  case 'N':
+                  case 'O':
+                        echo '6';
+                        break;
+                  case 'P':
+                  case 'Q':
+                  case 'R':
+                  case 'S':
+                        echo '7';
+                        break;
+                  case 'T':
+                  case 'U':
+                  case 'V':
+                        echo '8';
+                        break;
+                  case 'W':
+                  case 'X':
+                  case 'Y':
+                  case 'Z':
+                        echo '9';
+                        break;
+                  default:
+                        echo $lettre;
+            }
+      }
+}
+telephone("karifala");
+echo '<br>';
+telephone("Mohamed");
+echo '<br>';
+telephone("luca");
