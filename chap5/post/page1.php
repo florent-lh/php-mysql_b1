7870b35d28e120b7e58dcaa7e45d6a3d3ac23950
Florent <florent.email@email.com>
2025-10-24 18:39:43 +0200

    finalisation formulaire

--- chap5/post/page1.php
+++ chap5/post/page1.php
@@ -3,6 +3,8 @@
 // print_r($_SERVER);
 // echo '</pre>';
 
+//variable d'affichage
+$content = "";
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
       echo '<pre>';
       print_r($_POST);
@@ -18,10 +20,16 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
       $password = htmlspecialchars(trim($password));
 
       //vérification si les champs sont bien remplis :
-      if (empty($nom)) {
+      //nom et prénom : vérifier la longueur : entre 2 et 50 caractères
+      /*
+            si le nom est inférieur à 2 ou supérieur à 50
+                  message d'erreur : veuillez entrer votre nom
+      */
+      //SI la valeur du champ nom renseignée par l'utilisateur est inférieure à 2 OU supérieure à 50
+      if (iconv_strlen($nom) < 2 || iconv_strlen($nom) > 50) {
             $errorMessages['nom'] = 'Veuillez entrer votre nom.';
       }
-      if (empty($prenom)) {
+      if (iconv_strlen($prenom) < 2 || iconv_strlen($prenom) > 50) {
             $errorMessages['prenom'] = 'Veuillez entrer votre prénom.';
       }
       if (empty($age)) {
@@ -35,9 +43,20 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $errorMessages['email'] = 'Veuillez entrer votre email.';
       }
       if (empty($password)) {
+            //utiliser un preg_match : 1 caractère spécial, 1 majuscule, 1 minuscule, 1 chiffre, au moins 8 caractères
             $errorMessages['password'] = 'Veuillez entrer votre mot de passe.';
       }
+
+      //si le tableau d'erreurs est vide, le formulaire est valide
+      if (empty($errorMessages)) {
+            unset($_POST); //vider le formulaire
+            $content = '<div class="max-w-md mx-auto mt-10 p-6 bg-green-100 text-green-800 rounded-lg shadow-md">';
+            $content .= 'Formulaire validé avec succès !';
+            $content .= '</div>';
+      }
 }
+
+
 ?>
 
 <!DOCTYPE html>
@@ -52,27 +71,29 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 <body>
       <h1 class="text-3xl text-center">$_POST : superglobale pour les formulaires</h1>
+      <?= $content;  ?>
       <form action="<?= $_SERVER['PHP_SELF']; ?>" method="post" class="mx-auto max-w-md mt-10 p-6 rounded-lg shadow-md">
             <div class="flex flex-col">
                   <label for="nom" class="block text-gray-700 font-bold mb-2">Nom :</label>
-                  <input type="text" id="nom" name="nom" class="w-full px-3 py-2 border rounded-lg mb-4">
+                  <input type="text" id="nom" name="nom" class="w-full px-3 py-2 border rounded-lg mb-4" value="<?= $_POST['nom'] ?? null; ?>">
                   <span class="text-sm text-red-500 mb-4"><?= $errorMessages['nom'] ?? null; ?></span>
 
                   <label for="prenom" class="block text-gray-700 font-bold mb-2">Prénom :</label>
-                  <input type="text" id="prenom" name="prenom" class="w-full px-3 py-2 border rounded-lg mb-4">
+                  <input type="text" id="prenom" name="prenom" class="w-full px-3 py-2 border rounded-lg mb-4" value="<?= $_POST['prenom'] ?? null; ?>">
                   <span class="text-sm text-red-500 mb-4"><?= $errorMessages['prenom'] ?? null; ?></span>
 
                   <label for="age" class="block text-gray-700 font-bold mb-2">Âge :</label>
-                  <input type="number" id="age" name="age" class="w-full px-3 py-2 border rounded-lg mb-4">
+                  <input type="number" id="age" name="age" class="w-full px-3 py-2 border rounded-lg mb-4" value="<?= $_POST['age'] ?? null; ?>">
                   <span class="text-sm text-red-500 mb-4"><?= $errorMessages['age'] ?? null; ?></span>
 
                   <label for="email" class="block text-gray-700 font-bold mb-2">Email :</label>
-                  <input type="text" id="email" name="email" class="w-full px-3 py-2 border rounded-lg mb-4">
+                  <input type="text" id="email" name="email" class="w-full px-3 py-2 border rounded-lg mb-4" value="<?= $_POST['email'] ?? null; ?>">
                   <span class="text-sm text-red-500 mb-4"><?= $errorMessages['email'] ?? null; ?></span>
 
                   <label for="password" class="block text-gray-700 font-bold mb-2">Mot de passe :</label>
-                  <input type="password" id="password" name="password" class="w-full px-3 py-2 border rounded-lg mb-4">
+                  <input type="password" id="password" name="password" class="w-full px-3 py-2 border rounded-lg mb-4" value="<?= $_POST['password'] ?? null; ?>">
                   <span class="text-sm text-red-500 mb-4"><?= $errorMessages['password'] ?? null; ?></span>
+
                   <input type="submit" value="Envoyer" class="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 cursor-pointer">
             </div>
       </form>
